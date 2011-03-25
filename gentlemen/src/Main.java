c01dcdd17e35fdd2567642e1fac3b74e7b8eb4b0
Karl.Bristav@gmail.com <Karl.Bristav@gmail.com@d20b571a-298d-363c-a098-d6b3e227b0f4>
2011-03-25 18:47:40 +0000

    Added interface SubManager, as well as refactored packages.

--- gentlemen/src/Main.java
+++ gentlemen/src/Main.java
@@ -4,7 +4,6 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("hej");
 		System.out.println("HEJDÅ");
 	}
