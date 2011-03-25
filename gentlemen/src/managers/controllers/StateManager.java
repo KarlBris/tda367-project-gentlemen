9d5d7a1c941ad8eaa788ebc6e7cfba06b97d2318
gustav128@gmail.com <gustav128@gmail.com@d20b571a-298d-363c-a098-d6b3e227b0f4>
2011-03-25 19:06:00 +0000

    Added NetworkManager, StateManager, KeyboardManager and MouseManager class shells. Added managers.utilites package.

---
+++ gentlemen/src/managers/controllers/StateManager.java
@@ -0,0 +1,43 @@
+package managers.controllers;
+
+import core.SubManager;
+
+public class StateManager implements SubManager {
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void instantiatePermanentEntities() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void cleanup() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void entityAdded() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void entityRemoved() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
