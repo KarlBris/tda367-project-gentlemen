e3390d8a17a8f8ee09e2d76822aae7014dbd9fc0
gustav128@gmail.com <gustav128@gmail.com@d20b571a-298d-363c-a098-d6b3e227b0f4>
2011-03-25 16:37:24 +0000

    Added initial packages and the Entity class.

---
+++ trunk/gentlemen/src/core/Entity.java
@@ -0,0 +1,14 @@
+package core;
+
+/**
+ * 
+ * @author Gustav, Karl
+ *
+ */
+public abstract class Entity {
+	
+	public abstract void start();
+	public abstract void end();
+	public abstract void update();
+	public abstract void customRender();
+}
