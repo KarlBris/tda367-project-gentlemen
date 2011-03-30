eebd9bbd61c26c225a3a71ab69171ef41dcf9c1b
Karl.Bristav@gmail.com <Karl.Bristav@gmail.com@d20b571a-298d-363c-a098-d6b3e227b0f4>
2011-03-30 14:23:40 +0000

    Initialize() and Update() in RenderComponent implemented.

--- trunk/gentlemen/src/components/views/RenderComponent.java
+++ trunk/gentlemen/src/components/views/RenderComponent.java
@@ -1,13 +1,24 @@
 package components.views;
 
+import java.util.List;
+
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.GL11;
 import core.Component;
+import core.Entity;
+import core.Manager;
 
 public class RenderComponent implements Component {
 
 	@Override
 	public void initialize() {
-		// TODO Auto-generated method stub
-
+		
+		//Initialize the projection and matrix modes
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, 800, 600, 0, 1, -1);	//TODO Replace 800 and 600 with suitable units
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		
 	}
 
 	@Override
@@ -24,8 +35,22 @@ public class RenderComponent implements Component {
 
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
 
+		List<Entity> entityList = Manager.getEntities();
+		
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT| GL11.GL_DEPTH_BUFFER_BIT);
+		
+		GL11.glBegin(GL11.GL_TRIANGLES);
+		
+		for(Entity e : entityList) {
+			
+			if(e.getGeometry()!=null) {
+				e.getGeometry().render();
+			}
+			
+		}
+		
+		GL11.glEnd();
 	}
 
 	@Override
