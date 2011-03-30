de02e60a8aa09e800b298885f506d7f71e4e39dc
gustav128@gmail.com <gustav128@gmail.com@d20b571a-298d-363c-a098-d6b3e227b0f4>
2011-03-30 15:14:40 +0000

    Implemented the game loop in Manager.

--- gentlemen/src/Main.java
+++ gentlemen/src/Main.java
@@ -1,6 +1,4 @@
-import org.lwjgl.*;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
+import core.Manager;
 
 public class Main {
 
@@ -8,20 +6,7 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		try {
-			Display.setDisplayMode(new DisplayMode(800, 600));
-			Display.create();
-		}
-		catch (LWJGLException e) {
-			e.printStackTrace();
-			System.exit(0);
-		}
-		
-		while (!Display.isCloseRequested()) {
-			Display.update();
-		}
-		
-		Display.destroy();
+		Manager.start();
 	}
 
 }
