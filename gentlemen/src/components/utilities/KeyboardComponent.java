deee4c499a8e8822ac389011fda0a6435818f237
Karl.Bristav@gmail.com <Karl.Bristav@gmail.com@d20b571a-298d-363c-a098-d6b3e227b0f4>
2011-03-30 15:43:23 +0000

    Implemented keyboard-related methods getKey(), getKeyDown() and some logic in update()

--- gentlemen/src/components/utilities/KeyboardComponent.java
+++ gentlemen/src/components/utilities/KeyboardComponent.java
@@ -1,9 +1,38 @@
 package components.utilities;
 
+import org.lwjgl.input.Keyboard;
+
 import core.Component;
 
 public class KeyboardComponent implements Component {
 
+	// An array of booleans representing the current state of the keyboard
+	public static boolean[] keyDownArray = new boolean[Keyboard.KEYBOARD_SIZE];
+	
+	// An array of booleans representing the state of the keyboard before update() was called last time
+	public static boolean[] prevKeyDownArray = new boolean[Keyboard.KEYBOARD_SIZE];  
+	
+	
+	public static boolean getKey(int key) {
+		
+		// Returns the boolean value, from the current state of the keyboard, associated with the given key		
+		return keyDownArray[key];
+		
+	}
+	
+	public static boolean getKeyDown(int key) {		
+			
+		// If the given key is down right now, but wasn't down last update, the key just got pressed down, returning true
+		if(keyDownArray[key] && !prevKeyDownArray[key]) {
+			return true;
+		}
+		// In all other cases, return false
+		else {
+			return false;
+		}
+	}
+	
+	
 	@Override
 	public void initialize() {
 		// TODO Auto-generated method stub
@@ -24,7 +53,14 @@ public class KeyboardComponent implements Component {
 
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
+		
+		// Copy the current keyboard state to the previous
+		prevKeyDownArray = keyDownArray.clone();
+		
+		// Loop through the keys of the keyboard and update the current keyboard state accordingly 
+		for(int i = 0; i < keyDownArray.length; i++){
+			keyDownArray[i] = Keyboard.isKeyDown(i);
+		}
 
 	}
 
