7eb2aec85a62cb97d5812b4f27cd5285fac08ba6
gustav128@gmail.com <gustav128@gmail.com@d20b571a-298d-363c-a098-d6b3e227b0f4>
2011-03-30 11:52:40 +0000

    Set up LWJGL within the project. Experiment with how SVN handles external libraries.
    Edited Main to work as a small LWJGL test.

--- gentlemen/src/Main.java
+++ gentlemen/src/Main.java
@@ -1,13 +1,27 @@
+import org.lwjgl.*;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("hej");
-		System.out.println("och");
-		System.out.println("hå");
-		System.out.println("hopppppppp");
+		try {
+			Display.setDisplayMode(new DisplayMode(800, 600));
+			Display.create();
+		}
+		catch (LWJGLException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		while (!Display.isCloseRequested()) {
+			Display.update();
+		}
+		
+		Display.destroy();
 	}
 
 }
