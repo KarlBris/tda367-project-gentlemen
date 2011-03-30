097d8f4984b5ca48d9871197e42ae5f09152ba43
gustav128@gmail.com <gustav128@gmail.com@d20b571a-298d-363c-a098-d6b3e227b0f4>
2011-03-30 14:31:22 +0000

    Added components to Manager.

--- trunk/gentlemen/src/core/Manager.java
+++ trunk/gentlemen/src/core/Manager.java
@@ -4,14 +4,31 @@ import java.util.List;
 
 import models.Model;
 
-/**
- * 
- * @author Karl, Gustav
- *
- */
+import components.controllers.NetworkComponent;
+import components.controllers.PhysicsComponent;
+import components.controllers.StateComponent;
+import components.controllers.UpdateComponent;
+import components.utilities.KeyboardComponent;
+import components.utilities.MouseComponent;
+import components.views.RenderComponent;
+
 public class Manager {
 	
+	// Model
 	private static Model model = new Model();
+	
+	// Controllers
+	private static NetworkComponent network = new NetworkComponent();
+	private static PhysicsComponent physics = new PhysicsComponent();
+	private static StateComponent state = new StateComponent();
+	private static UpdateComponent update = new UpdateComponent();
+	
+	// View
+	private static RenderComponent render = new RenderComponent();
+	
+	// Utilities
+	private static KeyboardComponent keyboard = new KeyboardComponent();
+	private static MouseComponent mouse = new MouseComponent();
 	
 	private static void update() {
 		// TODO
