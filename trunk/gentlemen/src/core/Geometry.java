160c34a28cacdeefbc5135eecf67709c1ed3fe27
gustav128@gmail.com <gustav128@gmail.com@d20b571a-298d-363c-a098-d6b3e227b0f4>
2011-04-01 14:50:09 +0000

    Added class Constants.
    Worked on Geometry and RectangleGeometry.

--- trunk/gentlemen/src/core/Geometry.java
+++ trunk/gentlemen/src/core/Geometry.java
@@ -5,6 +5,10 @@ import org.lwjgl.util.Color;
 import org.lwjgl.util.vector.Vector2f;
 
 public abstract class Geometry {
+	
+	private Vector2f position;
+	private float angle;
+	
 	private Color color;
 	private Vector2f[] vertices;
 	private Vector2f[] uvs;
@@ -15,7 +19,55 @@ public abstract class Geometry {
 		this.uvs = uvs;
 	}
 	
+	public void setPosition(Vector2f position) {
+		this.position = position;
+	}
+	
+	public Vector2f getPosition() {
+		return position;
+	}
+	
+	public void setAngle(float angle) {
+		this.angle = angle % Constants.TWO_PI;
+	}
+	
+	public float getAngle() {
+		return angle;
+	}
+	
+	public void moveTowards(Vector2f targetPosition, float targetAngle) {
+		// Move position towards the targetPosition
+		Vector2f delta = new Vector2f();
+		Vector2f.sub(targetPosition, getPosition(), delta);
+		delta.scale(0.1f);
+		
+		Vector2f movement = new Vector2f();
+		Vector2f.add(getPosition(), delta, movement);
+		
+		Vector2f newPosition = new Vector2f();
+		Vector2f.add(position, movement, newPosition);
+		
+		setPosition(newPosition);
+		
+		// Move angle towards targetAngle
+		float directDelta = targetAngle - angle;
+		float indirectDelta = -Math.signum(directDelta) * (Constants.TWO_PI - directDelta);
+		
+		float angleMovement = 0.0f;
+		
+		if (Math.abs(directDelta) < Math.abs(indirectDelta)) {
+			angleMovement = directDelta;
+		}
+		else {
+			angleMovement = indirectDelta;
+		}
+		
+		setAngle(getAngle() + angleMovement);
+	}
+	
 	public void render() {
+		
+		// Set color
 		GL11.glColor3b(color.getRedByte(), color.getGreenByte(), color.getBlueByte());
 		
 		// Render the geometry as triangles
