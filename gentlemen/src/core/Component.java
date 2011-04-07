26f9ec993975078e01a85c1e94be4f6fe06e7fb3
gustav128@gmail.com <gustav128@gmail.com@d20b571a-298d-363c-a098-d6b3e227b0f4>
2011-04-07 09:26:45 +0000

    Added javadoc to Component.

--- gentlemen/src/core/Component.java
+++ gentlemen/src/core/Component.java
@@ -2,16 +2,36 @@ package core;
 
 public interface Component {
 	
+	/**
+	 * Allows the component to initialize itself at startup
+	 */
 	public void initialize();
 
+	/**
+	 * Allows the component to instantiate entities that it is responsible for
+	 */
 	public void instantiatePermanentEntities();
 	
+	/**
+	 * Allows the component perform cleanup tasks before the game exits
+	 */
 	public void cleanup();
 	
+	/**
+	 * Allows the component to perform tasks every frame of the game loop
+	 */
 	public void update();
 	
+	/**
+	 * Called when a new entity is instantiated
+	 * @param entity the newly created entity
+	 */
 	public void entityAdded(Entity entity);
 	
+	/**
+	 * Called when an entity is removed
+	 * @param entity the entity that is about to be removed
+	 */
 	public void entityRemoved(Entity entity);
 
 }
