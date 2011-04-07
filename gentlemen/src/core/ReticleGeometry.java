5e75a06e098f816d8a42fa68050332566a798300
erik.sikander@gmail.com <erik.sikander@gmail.com@d20b571a-298d-363c-a098-d6b3e227b0f4>
2011-04-07 09:36:40 +0000

    Added comments to ReticleGeometry

--- gentlemen/src/core/ReticleGeometry.java
+++ gentlemen/src/core/ReticleGeometry.java
@@ -1,27 +1,31 @@
 package core;
 
 import org.lwjgl.util.vector.Vector2f;
-
+/**
+ * ReticleGeometry represents the reticle geometry. 
+ *
+ */
 public class ReticleGeometry extends Geometry {
 	private static float lenght = 0.4f;
 	private static float width = 0.05f;
 	private static float distance = 0.1f;
 	
 	
-
+	// Sets the coordinates of the reticle geometry
 	private static Vector2f[] verticies = { new Vector2f(0.0f, -distance),  // Top triangle
 											new Vector2f(-width, -lenght),
 											new Vector2f(width, -lenght),
 											new Vector2f(distance, -0.0f),  // Right triangle
 											new Vector2f(lenght, -width),
 											new Vector2f(lenght, width),
-											new Vector2f(0.0f, distance),  // Bottom triangle
+											new Vector2f(0.0f, distance),  	// Bottom triangle
 											new Vector2f(width, lenght),
 											new Vector2f(-width, lenght),
-											new Vector2f(-distance, -0.0f),  // Left triangle
+											new Vector2f(-distance, -0.0f), // Left triangle
 											new Vector2f(-lenght, width),
 											new Vector2f(-lenght, -width)};
 	
+	// Sets the texture coordinates of the reticle geometry
 	private static Vector2f[] uvs = 	{   new Vector2f(),
 											new Vector2f(),
 											new Vector2f(),
