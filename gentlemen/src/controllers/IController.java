10a2c7178e61f61a8a8f0fc345e14a5ad867bd06
erik.sikander@gmail.com <erik.sikander@gmail.com@d20b571a-298d-363c-a098-d6b3e227b0f4>
2011-04-08 10:59:22 +0000

    Added comments on IController, and added a new interface IEntityFactory

--- gentlemen/src/controllers/IController.java
+++ gentlemen/src/controllers/IController.java
@@ -2,9 +2,29 @@ package controllers;
 
 import core.IModel;
 
+/**
+ * An interface which all controller will implement
+ */
 public interface IController {
+	
+	/**
+	 * Allows the controller to update itself and and the model in which the controller
+	 * is in control over
+	 */
 	public void update();
+	
+	/**
+	 * @return the model in which the controller is in control over
+	 */
 	public IModel getModel();
+	
+	/**
+	 * Allows the controller to preform task when it's created
+	 */
 	public void start();
+	
+	/**
+	 * Allows the controller to preform task when it's about to expire
+	 */
 	public void end();
 }
