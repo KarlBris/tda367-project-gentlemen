3d7c9bf84096c6db4a4c6377fa9534320dd94376
gustav128@gmail.com <gustav128@gmail.com@d20b571a-298d-363c-a098-d6b3e227b0f4>
2011-04-08 12:44:24 +0000

    Added ReticleModel.

---
+++ gentlemen/src/models/ReticleModel.java
@@ -0,0 +1,28 @@
+package models;
+
+import org.lwjgl.util.vector.Vector2f;
+
+import core.Geometry;
+import core.ReticleGeometry;
+
+public class ReticleModel implements IModel {
+	
+	private Geometry geometry = new ReticleGeometry();
+
+	@Override
+	public Geometry getGeometry() {
+		return geometry;
+	}
+	
+	public void setPosition(Vector2f position) {
+		geometry.setPosition(position);
+	}
+	
+	public void move(Vector2f movement) {
+		Vector2f newPosition = new Vector2f();
+		
+		Vector2f.add(geometry.getPosition(), movement, newPosition);
+		
+		geometry.setPosition(newPosition);
+	}
+}
