3345fbcbb9a9903e33a62fb4be0486220d5e82c5
Karl.Bristav@gmail.com <Karl.Bristav@gmail.com@d20b571a-298d-363c-a098-d6b3e227b0f4>
2011-04-08 14:17:07 +0000

    Fixed some comments and stuff

--- gentlemen/src/core/ReticleGeometry.java
+++ gentlemen/src/core/ReticleGeometry.java
@@ -9,22 +9,22 @@ import utilities.Color;
  * 
  */
 public class ReticleGeometry extends Geometry {
-	private static float lenght = 0.4f;
+	private static float length = 0.4f;
 	private static float width = 0.05f;
 	private static float distance = 0.1f;
 
 	// Sets the coordinates of the reticle geometry
 	private static Vector2f[] verticies = {
 			new Vector2f(0.0f, -distance), // Top triangle
-			new Vector2f(-width, -lenght),
-			new Vector2f(width, -lenght),
+			new Vector2f(-width, -length),
+			new Vector2f(width, -length),
 			new Vector2f(distance, -0.0f), // Right triangle
-			new Vector2f(lenght, -width),
-			new Vector2f(lenght, width),
+			new Vector2f(length, -width),
+			new Vector2f(length, width),
 			new Vector2f(0.0f, distance), // Bottom triangle
-			new Vector2f(width, lenght), new Vector2f(-width, lenght),
+			new Vector2f(width, length), new Vector2f(-width, length),
 			new Vector2f(-distance, -0.0f), // Left triangle
-			new Vector2f(-lenght, width), new Vector2f(-lenght, -width) };
+			new Vector2f(-length, width), new Vector2f(-length, -width) };
 
 	// Sets the texture coordinates of the reticle geometry
 	private static Vector2f[] uvs = { new Vector2f(), new Vector2f(),
