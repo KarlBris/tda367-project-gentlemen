3e53a0d8bb5d780cea8cdf3eb673d838b1010848
AKERSKUUUG <AKERSKUUUG@d20b571a-298d-363c-a098-d6b3e227b0f4>
2011-04-08 11:01:08 +0000

    Added Javadoc comments to IModel

--- gentlemen/src/core/IModel.java
+++ gentlemen/src/core/IModel.java
@@ -1,5 +1,17 @@
 package core;
 
+/**
+ * An interface which all models will implement
+ * 
+ */
 public interface IModel {
+
+	/**
+	 * Returns the Geometry object of the current Model.
+	 * 
+	 * @return the Geometry object of the current Model
+	 */
+
+	// Returns the Geometry object of the current Model
 	public Geometry getGeometry();
 }
