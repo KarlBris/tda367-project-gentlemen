6b319b608468d5932b11b6215f3eebb7565ef563
Karl.Bristav@gmail.com <Karl.Bristav@gmail.com@d20b571a-298d-363c-a098-d6b3e227b0f4>
2011-04-14 10:34:15 +0000

    Changed visibility for the boolean arrays in KeyboardComponent and MouseComponent to private. Also added 'final' modifier to all constants in Constants

--- gentlemen/src/core/Constants.java
+++ gentlemen/src/core/Constants.java
@@ -5,22 +5,22 @@ import org.lwjgl.util.Point;
 import org.lwjgl.util.vector.Vector2f;
 
 public class Constants {
-	
-	public static float PI = (float)Math.PI;
-	public static float TWO_PI = (float)(Math.PI * 2);
-	public static float TO_DEGREES = (float)(180.0 / Math.PI);
-	
-	public static int FRAMES_PER_SECOND = 60;
-	public static float DELTA_TIME = 1.0f / (float)FRAMES_PER_SECOND;
-	
-	public static int DEFAULT_SCREEN_WIDTH = 960;
-	public static int DEFAULT_SCREEN_HEIGHT = 540;
-	public static float VIEWPORT_RATIO = 16.0f / 9.0f;
-	public static float VIEWPORT_WIDTH = 22.0f;
-	public static float VIEWPORT_HEIGHT = VIEWPORT_WIDTH / VIEWPORT_RATIO;
-	
-	public static float GEOMETRY_TO_PHYSICS_INTERPOLATION = 0.1f;
-	
+
+	public static final float PI = (float) Math.PI;
+	public static final float TWO_PI = (float) (Math.PI * 2);
+	public static final float TO_DEGREES = (float) (180.0 / Math.PI);
+
+	public static final int FRAMES_PER_SECOND = 60;
+	public static final float DELTA_TIME = 1.0f / FRAMES_PER_SECOND;
+
+	public static final int DEFAULT_SCREEN_WIDTH = 960;
+	public static final int DEFAULT_SCREEN_HEIGHT = 540;
+	public static final float VIEWPORT_RATIO = 16.0f / 9.0f;
+	public static final float VIEWPORT_WIDTH = 22.0f;
+	public static final float VIEWPORT_HEIGHT = VIEWPORT_WIDTH / VIEWPORT_RATIO;
+
+	public static final float GEOMETRY_TO_PHYSICS_INTERPOLATION = 0.1f;
+
 	/**
 	 * @return the current screen width in pixels
 	 */
@@ -28,10 +28,10 @@ public class Constants {
 		if (Display.getDisplayMode() != null) {
 			return Display.getDisplayMode().getWidth();
 		}
-		
+
 		return 0;
 	}
-	
+
 	/**
 	 * @return the current screen height in pixels
 	 */
@@ -39,28 +39,35 @@ public class Constants {
 		if (Display.getDisplayMode() != null) {
 			return Display.getDisplayMode().getHeight();
 		}
-		
+
 		return 0;
 	}
-	
+
 	/**
 	 * Converts a pixel in screen-space to a point in viewport-space
-	 * @param pixelX x-coordinate in screen-space
-	 * @param pixelY y-coordinate in screen-space
+	 * 
+	 * @param pixelX
+	 *            x-coordinate in screen-space
+	 * @param pixelY
+	 *            y-coordinate in screen-space
 	 * @return a point in viewport-space
 	 */
-	public static Vector2f screenToViewport(int pixelX, int pixelY) {
-		return new Vector2f(((float)pixelX + 0.5f) / getScreenWidth() * VIEWPORT_WIDTH,
-							((float)pixelY + 0.5f) / getScreenHeight() * VIEWPORT_HEIGHT);
+	public static Vector2f screenToViewport(final int pixelX, final int pixelY) {
+		return new Vector2f(
+				(pixelX + 0.5f) / getScreenWidth() * VIEWPORT_WIDTH,
+				(pixelY + 0.5f) / getScreenHeight() * VIEWPORT_HEIGHT);
 	}
-	
+
 	/**
 	 * Converts a point in viewport-space to a pixel in screen-space
-	 * @param position a point in viewport-space
+	 * 
+	 * @param position
+	 *            a point in viewport-space
 	 * @return a pixel in screen-space
 	 */
-	public static Point viewportToScreen(Vector2f position) {
-		return new Point((int)(position.x / VIEWPORT_WIDTH * getScreenWidth()),
-						 (int)(position.y / VIEWPORT_HEIGHT * getScreenHeight()));
+	public static Point viewportToScreen(final Vector2f position) {
+		return new Point(
+				(int) (position.x / VIEWPORT_WIDTH * getScreenWidth()),
+				(int) (position.y / VIEWPORT_HEIGHT * getScreenHeight()));
 	}
 }
