84c46026bf83d628c3d6167e872b451a3382b454
Karl.Bristav@gmail.com <Karl.Bristav@gmail.com@d20b571a-298d-363c-a098-d6b3e227b0f4>
2011-05-03 14:48:30 +0000

    Fixed misleading and/or somewhat sparse comments in the RenderComponent and Color classes. Added and implemented angleToVector() method in the Tools class. Also implemented some more temporary gameplay mechanics in the LocalPlayerController class. Changed the interpolation rate to 0.8f.

--- gentlemen/src/components/RenderComponent.java
+++ gentlemen/src/components/RenderComponent.java
@@ -10,45 +10,46 @@ import controllers.IController;
 import core.Manager;
 
 /**
- * This class is responsible for setting up the viewport projection and making Entities render themselves 
+ * This class is responsible for setting up the viewport projection and making
+ * Entities render themselves
  */
 public class RenderComponent implements IComponent {
 
-	
 	/**
-	 * Initializes the viewport projection based upon the aspect ratio of the display window
+	 * Initializes the viewport projection based upon the aspect ratio of the
+	 * display window
 	 * 
 	 * @see components.IComponent#initialize()
 	 */
 	@Override
 	public void initialize() {
-		
+
 		// The width and height of the viewport in game units
 		float width;
 		float height;
 
-		
 		// Initializes the display window size
-		int displayHeight = core.Constants.getScreenHeight();
-		int displayWidth = core.Constants.getScreenWidth();
-		
+		final int displayHeight = core.Constants.getScreenHeight();
+		final int displayWidth = core.Constants.getScreenWidth();
+
 		// Calculate the aspect ratio of the display window
-		float displayRatio = (float) displayWidth / (float) displayHeight;
-		
-		
-		// If the screen is slimmer than the standard ratio (16:9), make a new, smaller, width
-		// but keep the height
-		if(displayRatio < core.Constants.VIEWPORT_RATIO) {
-			width = (core.Constants.VIEWPORT_HEIGHT / displayHeight) * displayWidth;
+		final float displayRatio = (float) displayWidth / (float) displayHeight;
+
+		// If the screen is slimmer than the standard ratio (16:9), make a new,
+		// smaller, width but keep the standard height
+		if (displayRatio < core.Constants.VIEWPORT_RATIO) {
+			width = (core.Constants.VIEWPORT_HEIGHT / displayHeight)
+					* displayWidth;
 			height = core.Constants.VIEWPORT_HEIGHT;
 		}
-		// If the screen is wider than or equally wide to the standard ratio, keep the height
-		// and make a new, smaller, width
+		// If the screen is wider than or equally wide to the standard ratio,
+		// keep the standard width and make a new, smaller, height
 		else {
 			width = core.Constants.VIEWPORT_WIDTH;
-			height = (core.Constants.VIEWPORT_WIDTH / displayWidth) * displayHeight;
+			height = (core.Constants.VIEWPORT_WIDTH / displayWidth)
+					* displayHeight;
 		}
-		
+
 		// Initialize the projection and matrix modes
 		GL11.glMatrixMode(GL11.GL_PROJECTION);
 		GL11.glLoadIdentity();
@@ -79,17 +80,17 @@ public class RenderComponent implements IComponent {
 	 */
 	@Override
 	public void update() {
-		
-		List<IModel> models = Manager.getModels();
+
+		final List<IModel> models = Manager.getModels();
 
 		GL11.glGetError();
-		
+
 		// Clear the color buffer and depth buffer
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-		
+
 		// Let each entity render itself
-		for(IModel m : models) {
-			
+		for (final IModel m : models) {
+
 			m.getGeometry().render();
 		}
 	}
@@ -98,7 +99,7 @@ public class RenderComponent implements IComponent {
 	 * @see components.IComponent#controllerAdded(core.Entity)
 	 */
 	@Override
-	public void controllerAdded(IController controller) {
+	public void controllerAdded(final IController controller) {
 		// TODO Auto-generated method stub
 	}
 
@@ -106,7 +107,7 @@ public class RenderComponent implements IComponent {
 	 * @see components.IComponent#controllerRemoved(core.Entity)
 	 */
 	@Override
-	public void controllerRemoved(IController controller) {
+	public void controllerRemoved(final IController controller) {
 		// TODO Auto-generated method stub
 	}
 
