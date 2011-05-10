22dec11b7cf78b67f183d42a1a33f8f94f45425b
gustav128@gmail.com <gustav128@gmail.com@d20b571a-298d-363c-a098-d6b3e227b0f4>
2011-05-10 12:11:00 +0000

    Cleaned up Tools and ToolsTest classes.

--- trunk/gentlemen/test/utilities/ToolsTest.java
+++ trunk/gentlemen/test/utilities/ToolsTest.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package utilities;
 
 import static org.junit.Assert.assertTrue;
@@ -12,10 +9,6 @@ import org.junit.Test;
 import org.lwjgl.util.Point;
 import org.lwjgl.util.vector.Vector2f;
 
-/**
- * @author Sickan
- * 
- */
 public class ToolsTest {
 
 	private final float precision = 0.01f;
@@ -34,13 +27,28 @@ public class ToolsTest {
 	public void tearDown() throws Exception {
 	}
 
+	@Test
+	public void testClampValue() {
+		// Test a value inside the clamp range
+		assertTrue(Tools.clampValue(7.0f, 0.0f, 10.0f) - 7.0f <= precision);
+
+		// Test a value greater than the clamp range
+		assertTrue(Tools.clampValue(15.0f, 0.0f, 10.0f) - 10.0f <= precision);
+
+		// Test a value less than the clamp range
+		assertTrue(Tools.clampValue(-23.0f, 0.0f, 10.0f) - 0.0f <= precision);
+
+		// Test a value less than the negative clamp value
+		assertTrue(Tools.clampValue(-23.0f, -10.0f, 10.0f) + 10.0f <= precision);
+	}
+
 	/**
 	 * Test method for {@link utilities.Tools#wrapAngle(float)}.
 	 */
 	@Test
 	public void testWrapAngle() {
 
-		// Test it change the angle to between 0 and two pi
+		// Test a value of 3*PI
 		float testAngle = Constants.PI + Constants.TWO_PI;
 		assertTrue(Math.abs(Tools.wrapAngle(testAngle) - Constants.PI) <= precision);
 
@@ -48,7 +56,8 @@ public class ToolsTest {
 		testAngle = -Constants.PI;
 		assertTrue(Math.abs(Tools.wrapAngle(testAngle) - Constants.PI) <= precision);
 
-		// Test that there is no change if it's correct
+		// Test that there is no change when the value is already in the correct
+		// range
 		testAngle = Constants.PI;
 		assertTrue(Math.abs(Tools.wrapAngle(testAngle) - Constants.PI) <= precision);
 
@@ -60,35 +69,35 @@ public class ToolsTest {
 	@Test
 	public void testClosestAngleDelta() {
 
-		// Test if it takes the closes angle and it's correct sign
-		float testAngleOne = 0.0f;
-		float testAngleTwo = Constants.TWO_PI * 0.8f;
+		// Test the angle delta magnitude and sign; positive movement
+		float testAngleOne = Constants.PI / 2;
+		float testAngleTwo = Constants.PI;
 		float closestAngle = Tools
 				.closestAngleDelta(testAngleOne, testAngleTwo);
-		assertTrue(Math.abs(Math.abs(closestAngle) - Constants.TWO_PI * 0.2f) <= precision
-				&& closestAngle <= 0);
+		assertTrue(Math.abs(Math.abs(closestAngle) - Constants.PI / 2) <= precision);
+		assertTrue(closestAngle > 0.0f);
 
-		// Test the method considers the possibility the angle ain't in the
-		// range 0.0 to Pi
+		// Test the angle delta magnitude and sign; negative movement
+		testAngleOne = Constants.PI;
+		testAngleTwo = Constants.PI / 2;
+		closestAngle = Tools.closestAngleDelta(testAngleOne, testAngleTwo);
+		assertTrue(Math.abs(Math.abs(closestAngle) - Constants.PI / 2) <= precision);
+		assertTrue(closestAngle < 0.0f);
+
+		// Test the angle delta magnitude and sign where it should wrap across
+		// angle 0; negative movement
+		testAngleOne = Constants.TWO_PI * 0.2f;
+		testAngleTwo = Constants.TWO_PI * 0.8f;
+		closestAngle = Tools.closestAngleDelta(testAngleOne, testAngleTwo);
+		assertTrue(Math.abs(Math.abs(closestAngle) - Constants.TWO_PI * 0.4f) <= precision);
+		assertTrue(closestAngle < 0.0f);
+
+		// Test angles that are not in the standard range 0 to TWO_PI
 		testAngleOne = 0.0f;
 		testAngleTwo = Constants.TWO_PI + Constants.PI;
 		closestAngle = Tools.closestAngleDelta(testAngleOne, testAngleTwo);
 		assertTrue(Math.abs(Math.abs(closestAngle) - Constants.PI) <= precision);
-
-		// Test the closest angle is positive
-		testAngleOne = Constants.PI / 2;
-		testAngleTwo = Constants.PI;
-		closestAngle = Tools.closestAngleDelta(testAngleOne, testAngleTwo);
-		assertTrue(Math.abs(Math.abs(closestAngle) - Constants.PI / 2) <= precision
-				&& closestAngle >= 0);
-
-		// Test the closest angle is negative
-		testAngleOne = Constants.PI;
-		testAngleTwo = Constants.PI / 2;
-		closestAngle = Tools.closestAngleDelta(testAngleOne, testAngleTwo);
-		assertTrue(Math.abs(Math.abs(closestAngle) - Constants.PI / 2) <= precision
-				&& closestAngle <= 0);
-
+		assertTrue(closestAngle > 0.0f);
 	}
 
 	/**
@@ -100,45 +109,45 @@ public class ToolsTest {
 		// Test an angle of 0.0f should produce a vector {1.0, 0.0}
 		float testAngle = 0.0f;
 		Vector2f testVector = Tools.angleToVector(testAngle);
-		assertTrue(Math.abs(testVector.x - 1.0f) <= precision
-				&& Math.abs(testVector.y - 0.0f) <= precision);
+		assertTrue(Math.abs(testVector.x - 1.0f) <= precision);
+		assertTrue(Math.abs(testVector.y - 0.0f) <= precision);
 
 		// Test an angle of Pi should produce a vector {-1.0, 0.0}
 		testAngle = Constants.PI;
 		testVector = Tools.angleToVector(testAngle);
-		assertTrue(Math.abs(testVector.x + 1.0f) <= precision
-				&& Math.abs(testVector.y - 0.0f) <= precision);
+		assertTrue(Math.abs(testVector.x + 1.0f) <= precision);
+		assertTrue(Math.abs(testVector.y - 0.0f) <= precision);
 
 		// Test an angle of 1.5 Pi should produce a vector {0.0, 1.0}
 		testAngle = Constants.TWO_PI * 3 / 4;
 		testVector = Tools.angleToVector(testAngle);
-		assertTrue(Math.abs(testVector.x - 0.0f) <= precision
-				&& Math.abs(testVector.y - 1.0f) <= precision);
+		assertTrue(Math.abs(testVector.x - 0.0f) <= precision);
+		assertTrue(Math.abs(testVector.y - 1.0f) <= precision);
 
 		// Test an angle of Pi/2 should produce a vector {0.0, -1.0}
 		testAngle = Constants.PI / 2;
 		testVector = Tools.angleToVector(testAngle);
-		assertTrue(Math.abs(testVector.x - 0.0f) <= precision
-				&& Math.abs(testVector.y + 1.0f) <= precision);
+		assertTrue(Math.abs(testVector.x - 0.0f) <= precision);
+		assertTrue(Math.abs(testVector.y + 1.0f) <= precision);
 
 		// Test an angle of three Pi should produce a vector {-1.0, 0.0}
 		testAngle = Constants.PI + Constants.TWO_PI;
 		testVector = Tools.angleToVector(testAngle);
-		assertTrue(Math.abs(testVector.x + 1.0f) <= precision
-				&& Math.abs(testVector.y - 0.0f) <= precision);
+		assertTrue(Math.abs(testVector.x + 1.0f) <= precision);
+		assertTrue(Math.abs(testVector.y - 0.0f) <= precision);
 
 		// Test an angle of -pi should produce a vector {-1.0, 0.0}
 		testAngle = -Constants.PI;
 		testVector = Tools.angleToVector(testAngle);
-		assertTrue(Math.abs(testVector.x + 1.0f) <= precision
-				&& Math.abs(testVector.y - 0.0f) <= precision);
+		assertTrue(Math.abs(testVector.x + 1.0f) <= precision);
+		assertTrue(Math.abs(testVector.y - 0.0f) <= precision);
 
 		// Test an angle of Pi / 4 should produce a vector {1/sqrt(2),
 		// -1/sqrt(2)}
 		testAngle = Constants.PI / 4;
 		testVector = Tools.angleToVector(testAngle);
-		assertTrue(Math.abs(testVector.x - 1 / Math.sqrt(2)) <= precision
-				&& Math.abs(testVector.y + 1 / Math.sqrt(2)) <= precision);
+		assertTrue(Math.abs(testVector.x - 1 / Math.sqrt(2)) <= precision);
+		assertTrue(Math.abs(testVector.y + 1 / Math.sqrt(2)) <= precision);
 	}
 
 	/**
@@ -148,27 +157,25 @@ public class ToolsTest {
 	@Test
 	public void testVectorToAngle() {
 
-		// Test an vector of {1/sqrt(2), -1/sqrt(2)} should produce a angle of
-		// pi/4
+		// Test a vector of {1/sqrt(2), -1/sqrt(2)} should produce a angle of
+		// PI/4
 		Vector2f testVector = new Vector2f(1.0f / (float) Math.sqrt(2), -1.0f
 				/ (float) Math.sqrt(2));
 		float testAngle = Tools.vectorToAngle(testVector);
-		assertTrue(Math.abs(Math.abs(testAngle) - Constants.PI / 4.0f) <= precision
-				&& testAngle >= 0.0f);
+		assertTrue(Math.abs(Math.abs(testAngle) - Constants.PI / 4.0f) <= precision);
+		assertTrue(testAngle > 0.0f);
 
-		// Test an vector of {2.0f, 0.0f} should produce a angle of 0.0
+		// Test a vector of {2.0f, 0.0f} should produce a angle of 0.0
 		testVector = new Vector2f(2.0f, 0.0f);
 		testAngle = Tools.vectorToAngle(testVector);
-		assertTrue(Math.abs(Math.abs(testAngle) - 0.0f) <= precision
-				&& testAngle >= 0.0f);
+		assertTrue(Math.abs(Math.abs(testAngle) - 0.0f) <= precision);
 
-		// Test an vector of {0.0f, 1.0f} should produce a angle of 6.0 *
-		// Pi/4.0f
+		// Test a vector of {0.0f, 1.0f} should produce a angle of 1.5 * PI
 		testVector = new Vector2f(0.0f, 1.0f);
 		testAngle = Tools.vectorToAngle(testVector);
 		assertTrue(Math.abs(Math.abs(testAngle) - Constants.TWO_PI * 3.0f
-				/ 4.0f) <= precision
-				&& testAngle >= 0.0f);
+				/ 4.0f) <= precision);
+		assertTrue(testAngle > 0.0f);
 	}
 
 	/**
@@ -184,25 +191,28 @@ public class ToolsTest {
 		Vector2f testVectorTwo = new Vector2f(5.0f, 3.0f);
 		Vector2f vectorBetween = Tools.vectorBetween(testVectorOne,
 				testVectorTwo);
-		assertTrue(Math.abs(vectorBetween.x - 5.0f) <= precision
-				&& Math.abs(vectorBetween.y - 3.0f) <= precision
-				&& vectorBetween.x >= 0.0f && vectorBetween.y >= 0.0f);
+		assertTrue(Math.abs(vectorBetween.x - 5.0f) <= precision);
+		assertTrue(Math.abs(vectorBetween.y - 3.0f) <= precision);
+		assertTrue(vectorBetween.x > 0.0f);
+		assertTrue(vectorBetween.y > 0.0f);
 
 		// Test two
 		testVectorOne = new Vector2f(0.0f, -1.0f);
 		testVectorTwo = new Vector2f(-5.0f, 3.0f);
 		vectorBetween = Tools.vectorBetween(testVectorOne, testVectorTwo);
-		assertTrue(Math.abs(vectorBetween.x + 5.0f) <= precision
-				&& Math.abs(vectorBetween.y - 4.0f) <= precision
-				&& vectorBetween.x <= 0.0f && vectorBetween.y >= 0.0f);
+		assertTrue(Math.abs(vectorBetween.x + 5.0f) <= precision);
+		assertTrue(Math.abs(vectorBetween.y - 4.0f) <= precision);
+		assertTrue(vectorBetween.x < 0.0f);
+		assertTrue(vectorBetween.y > 0.0f);
 
 		// Test three
 		testVectorOne = new Vector2f(0.0f, -1.0f);
 		testVectorTwo = new Vector2f(-5.0f, 3.0f);
 		vectorBetween = Tools.vectorBetween(testVectorOne, testVectorTwo);
-		assertTrue(Math.abs(vectorBetween.x + 5.0f) <= precision
-				&& Math.abs(vectorBetween.y - 4.0f) <= precision
-				&& vectorBetween.x <= 0.0f && vectorBetween.y >= 0.0f);
+		assertTrue(Math.abs(vectorBetween.x + 5.0f) <= precision);
+		assertTrue(Math.abs(vectorBetween.y - 4.0f) <= precision);
+		assertTrue(vectorBetween.x < 0.0f);
+		assertTrue(vectorBetween.y > 0.0f);
 	}
 
 	/**
@@ -241,8 +251,8 @@ public class ToolsTest {
 
 		Vector2f testVector = new Vector2f(-2.0f, 4.0f);
 		Vec2 convertedVector = Tools.toPhysicsVector(testVector);
-		assertTrue(testVector.x == convertedVector.x
-				&& testVector.y == convertedVector.y);
+		assertTrue(testVector.x == convertedVector.x);
+		assertTrue(testVector.y == convertedVector.y);
 
 	}
 
@@ -252,10 +262,12 @@ public class ToolsTest {
 	 */
 	@Test
 	public void testToNormalVector() {
+
 		Vec2 testVector = new Vec2(-2.0f, 4.0f);
 		Vector2f convertedVector = Tools.toNormalVector(testVector);
-		assertTrue(testVector.x == convertedVector.x
-				&& testVector.y == convertedVector.y);
+		assertTrue(testVector.x == convertedVector.x);
+		assertTrue(testVector.y == convertedVector.y);
+
 	}
 
 	/**
@@ -264,10 +276,12 @@ public class ToolsTest {
 	 */
 	@Test
 	public void testCloneVector() {
+
 		Vector2f testVector = new Vector2f(-2.0f, 4.0f);
 		Vector2f clonedVector = Tools.cloneVector(testVector);
-		assertTrue(testVector != clonedVector && testVector.y == clonedVector.y
-				&& testVector.x == clonedVector.x);
+		assertTrue(testVector != clonedVector);
+		assertTrue(testVector.x == clonedVector.x);
+		assertTrue(testVector.y == clonedVector.y);
 	}
 
 	/**
@@ -275,11 +289,15 @@ public class ToolsTest {
 	 */
 	@Test
 	public void testScreenToViewport() {
+
+		// Test if the viewport aspect ratio is the same as the screen aspect
+		// ratio
 		Vector2f testViewport = Tools.screenToViewport(Tools.getScreenWidth(),
-				Tools.getScreenWidth());
-		assertTrue(testViewport.x / testViewport.y
-				- (float) Tools.getScreenWidth()
-				/ (float) Tools.getScreenHeight() <= precision);
+				Tools.getScreenHeight());
+
+		assertTrue(Math.abs(testViewport.x / testViewport.y
+				- ((float) Constants.DEFAULT_SCREEN_WIDTH)
+				/ Constants.DEFAULT_SCREEN_HEIGHT) <= precision);
 	}
 
 	/**
@@ -288,7 +306,10 @@ public class ToolsTest {
 	 */
 	@Test
 	public void testViewportToScreen() {
+		// Test if the viewport aspect ratio is the same as the aspect ratio of
+		// the input
 		Point testScreen = Tools.viewportToScreen(new Vector2f(8.0f, 4.0f));
+
 		assertTrue((float) testScreen.getX() / (float) testScreen.getY() - 8.0f
 				/ 4.0f <= precision);
 	}
