89b3a6c43d80c8af09d8f50352cec368d3abc987
gustav128@gmail.com <gustav128@gmail.com@d20b571a-298d-363c-a098-d6b3e227b0f4>
2011-05-16 10:32:56 +0000

    Added BodyNotInitializedException.
    Body handling is now safer.
    
    Not yet changed the rest of the project to reflect this change.

--- gentlemen/src/common/body/Body.java
+++ gentlemen/src/common/body/Body.java
@@ -13,19 +13,20 @@ import utilities.Tools;
 /**
  * The physical representation of a model in the game world
  */
-public class Body {
+public class Body implements IBody {
 
 	// Body properties
+	private final IBodyShape shape;
+	private final boolean isStatic;
+
 	private float mass = 0.0f;
 	private float damping = Constants.BODY_DEFAULT_DAMPING;
 	private float angularDamping = Constants.BODY_DEFAULT_ANGULAR_DAMPING;
 
-	private final IBodyShape shape;
-
 	// External references
-	private org.jbox2d.dynamics.Body rigidbody;
-
 	private IBodyCollisionCallback collisionCallback;
+
+	private org.jbox2d.dynamics.Body rigidbody;
 
 	/**
 	 * Initializes a static/dynamic body
@@ -39,6 +40,13 @@ public class Body {
 	 */
 	public Body(final IBodyShape shape, final float mass) {
 		this.shape = shape;
+
+		if (mass == 0.0f) {
+			this.isStatic = true;
+		} else {
+			this.isStatic = false;
+		}
+
 		this.mass = mass;
 	}
 
@@ -61,57 +69,10 @@ public class Body {
 	}
 
 	/**
-	 * Adds the body to a physics world
-	 * 
-	 * @param world
-	 *            the world
+	 * @return false if the body is movable, true otherwise
 	 */
-	public void addToWorld(final World world) {
-		final BodyDef bodyDef = new BodyDef();
-
-		if (mass == 0.0f) {
-			bodyDef.type = BodyType.STATIC;
-		} else {
-			bodyDef.type = BodyType.DYNAMIC;
-		}
-
-		rigidbody = world.createBody(bodyDef);
-
-		rigidbody.setUserData(this);
-
-		rigidbody.createFixture(shape.getShape(), 1.0f);
-
-		setMass(mass);
-		setDamping(damping);
-		setAngularDamping(angularDamping);
-	}
-
-	/**
-	 * Removes the body from the physics world
-	 * 
-	 * @param world
-	 *            the world
-	 */
-	public void removeFromWorld(final World world) {
-		world.destroyBody(rigidbody);
-	}
-
-	/**
-	 * @return the collision callback object this body reports to
-	 */
-	public IBodyCollisionCallback getCollisionCallback() {
-		return collisionCallback;
-	}
-
-	/**
-	 * Sets the collision callback object this body will report to
-	 * 
-	 * @param collisionCallback
-	 *            the collision callback object
-	 */
-	public void setCollisionCallback(
-			final IBodyCollisionCallback collisionCallback) {
-		this.collisionCallback = collisionCallback;
+	public boolean isStatic() {
+		return isStatic;
 	}
 
 	/**
@@ -128,13 +89,14 @@ public class Body {
 	 *            the new mass
 	 */
 	public void setMass(final float mass) {
-		// Set mass
-		if (this.mass != 0.0f) {
-			this.mass = mass;
+		if (rigidbody == null) {
+			throw new BodyNotInitializedException();
 		}
 
-		// Update rigidbody
-		if (rigidbody != null && rigidbody.getType() == BodyType.DYNAMIC) {
+		// Set mass
+		if (!this.isStatic) {
+			this.mass = mass;
+
 			final MassData data = new MassData();
 
 			rigidbody.getMassData(data);
@@ -157,13 +119,14 @@ public class Body {
 	 *            the linear damping
 	 */
 	public void setDamping(final float damping) {
-		// Update damping
+		if (rigidbody == null) {
+			throw new BodyNotInitializedException();
+		}
+
+		// Set damping
 		this.damping = damping;
 
-		// Update rigidbody
-		if (rigidbody != null) {
-			rigidbody.setLinearDamping(this.damping);
-		}
+		rigidbody.setLinearDamping(this.damping);
 	}
 
 	/**
@@ -180,24 +143,43 @@ public class Body {
 	 *            the angular damping
 	 */
 	public void setAngularDamping(final float angularDamping) {
-		// Update angular damping
+		if (rigidbody == null) {
+			throw new BodyNotInitializedException();
+		}
+
+		// Set angular damping
 		this.angularDamping = angularDamping;
 
-		// Update rigidbody
-		if (rigidbody != null) {
-			rigidbody.setAngularDamping(this.angularDamping);
-		}
+		rigidbody.setAngularDamping(this.angularDamping);
+	}
+
+	/**
+	 * @return the collision callback object this body reports to
+	 */
+	public IBodyCollisionCallback getCollisionCallback() {
+		return collisionCallback;
+	}
+
+	/**
+	 * Sets the collision callback object this body will report to
+	 * 
+	 * @param collisionCallback
+	 *            the collision callback object
+	 */
+	public void setCollisionCallback(
+			final IBodyCollisionCallback collisionCallback) {
+		this.collisionCallback = collisionCallback;
 	}
 
 	/**
 	 * @return the body's current position
 	 */
 	public Vector2f getPosition() {
-		if (rigidbody != null) {
-			return Tools.toNormalVector(rigidbody.getWorldCenter());
+		if (rigidbody == null) {
+			throw new BodyNotInitializedException();
 		}
 
-		return new Vector2f(0.0f, 0.0f);
+		return Tools.toNormalVector(rigidbody.getWorldCenter());
 	}
 
 	/**
@@ -207,21 +189,23 @@ public class Body {
 	 *            the new position
 	 */
 	public void setPosition(final Vector2f position) {
-		if (rigidbody != null) {
-			rigidbody.setTransform(Tools.toPhysicsVector(position),
-					rigidbody.getAngle());
+		if (rigidbody == null) {
+			throw new BodyNotInitializedException();
 		}
+
+		rigidbody.setTransform(Tools.toPhysicsVector(position),
+				rigidbody.getAngle());
 	}
 
 	/**
 	 * @return the current angle of the body
 	 */
 	public float getAngle() {
-		if (rigidbody != null) {
-			return Tools.wrapAngle(-rigidbody.getAngle());
+		if (rigidbody == null) {
+			throw new BodyNotInitializedException();
 		}
 
-		return 0.0f;
+		return Tools.wrapAngle(-rigidbody.getAngle());
 	}
 
 	/**
@@ -231,28 +215,71 @@ public class Body {
 	 *            the new angle
 	 */
 	public void setAngle(final float angle) {
-		if (rigidbody != null) {
-			rigidbody.setTransform(rigidbody.getWorldCenter(), -angle);
+		if (rigidbody == null) {
+			throw new BodyNotInitializedException();
+		}
+
+		rigidbody.setTransform(rigidbody.getWorldCenter(), -angle);
+	}
+
+	@Override
+	public void addToWorld(final World world) {
+		// Create body definition
+		final BodyDef bodyDef = new BodyDef();
+
+		if (isStatic) {
+			bodyDef.type = BodyType.STATIC;
+		} else {
+			bodyDef.type = BodyType.DYNAMIC;
+		}
+
+		// Create rigidbody
+		rigidbody = world.createBody(bodyDef);
+
+		rigidbody.setUserData(this);
+
+		// Set body properties
+		rigidbody.createFixture(shape.getShape(), 1.0f);
+
+		try {
+
+			setMass(mass);
+			setDamping(damping);
+			setAngularDamping(angularDamping);
+
+		} catch (BodyNotInitializedException e) {
+			e.printStackTrace();
 		}
 	}
 
+	@Override
+	public void removeFromWorld(final World world) {
+		world.destroyBody(rigidbody);
+	}
+
+	/**
+	 * @return the current acceleration of the body
+	 */
 	public Vector2f getAcceleration() {
-		if (rigidbody != null) {
-			final Vector2f acceleration = Tools
-					.toNormalVector(rigidbody.m_force);
-
-			acceleration.scale(1.0f / getMass());
-
-			return acceleration;
+		if (rigidbody == null) {
+			throw new BodyNotInitializedException();
 		}
 
-		return new Vector2f(0.0f, 0.0f);
+		final Vector2f acceleration = Tools.toNormalVector(rigidbody.m_force);
+
+		acceleration.scale(1.0f / getMass());
+
+		return acceleration;
 	}
 
 	/**
 	 * @return the current linear velocity of the body
 	 */
 	public Vector2f getVelocity() {
+		if (rigidbody == null) {
+			throw new BodyNotInitializedException();
+		}
+
 		return Tools.toNormalVector(rigidbody.getLinearVelocity());
 	}
 
@@ -264,34 +291,38 @@ public class Body {
 	 * @return the velocity
 	 */
 	public Vector2f getVelocityAtPoint(final Vector2f point) {
-		if (rigidbody != null) {
-			final Vec2 velocityAtPoint = new Vec2(0.0f, 0.0f);
-
-			rigidbody.getLinearVelocityFromWorldPointToOut(
-					Tools.toPhysicsVector(point), velocityAtPoint);
-
-			return Tools.toNormalVector(velocityAtPoint);
+		if (rigidbody == null) {
+			throw new BodyNotInitializedException();
 		}
 
-		return new Vector2f(0.0f, 0.0f);
+		final Vec2 velocityAtPoint = new Vec2(0.0f, 0.0f);
+
+		rigidbody.getLinearVelocityFromWorldPointToOut(
+				Tools.toPhysicsVector(point), velocityAtPoint);
+
+		return Tools.toNormalVector(velocityAtPoint);
 	}
 
 	/**
 	 * Clears the linear velocity of the body
 	 */
 	public void clearVelocity() {
-		if (rigidbody != null) {
-			rigidbody.setLinearVelocity(new Vec2(0.0f, 0.0f));
+		if (rigidbody == null) {
+			throw new BodyNotInitializedException();
 		}
+
+		rigidbody.setLinearVelocity(new Vec2(0.0f, 0.0f));
 	}
 
 	/**
 	 * Clears the angular velocity of the body
 	 */
 	public void clearAngularVelocity() {
-		if (rigidbody != null) {
-			rigidbody.setAngularVelocity(0.0f);
+		if (rigidbody == null) {
+			throw new BodyNotInitializedException();
 		}
+
+		rigidbody.setAngularVelocity(0.0f);
 	}
 
 	/**
@@ -301,10 +332,12 @@ public class Body {
 	 *            the force
 	 */
 	public void applyForce(final Vector2f force) {
-		if (rigidbody != null) {
-			rigidbody.applyForce(Tools.toPhysicsVector(force),
-					rigidbody.getWorldCenter());
+		if (rigidbody == null) {
+			throw new BodyNotInitializedException();
 		}
+
+		rigidbody.applyForce(Tools.toPhysicsVector(force),
+				rigidbody.getWorldCenter());
 	}
 
 	/**
@@ -314,9 +347,11 @@ public class Body {
 	 *            the torque
 	 */
 	public void applyTorque(final float torque) {
-		if (rigidbody != null) {
-			rigidbody.applyTorque(-torque);
+		if (rigidbody == null) {
+			throw new BodyNotInitializedException();
 		}
+
+		rigidbody.applyTorque(-torque);
 	}
 
 	/**
@@ -326,13 +361,14 @@ public class Body {
 	 *            the velocity to add
 	 */
 	public void applyVelocityChange(final Vector2f velocity) {
-		if (rigidbody != null) {
-			final Vec2 impulse = Tools.toPhysicsVector(velocity);
-
-			impulse.mul(mass);
-
-			rigidbody.applyLinearImpulse(impulse, rigidbody.getWorldCenter());
+		if (rigidbody == null) {
+			throw new BodyNotInitializedException();
 		}
-	}
 
+		final Vec2 impulse = Tools.toPhysicsVector(velocity);
+
+		impulse.mul(mass);
+
+		rigidbody.applyLinearImpulse(impulse, rigidbody.getWorldCenter());
+	}
 }
---
+++ gentlemen/src/common/body/BodyNotInitializedException.java
@@ -0,0 +1,10 @@
+package common.body;
+
+public class BodyNotInitializedException extends RuntimeException {
+
+	/**
+	 * Exception is serializable
+	 */
+	private static final long serialVersionUID = 3557894192747028974L;
+
+}
