0f0f73843ffacd4d94b9410a0898f487df90d7ea
Karl.Bristav@gmail.com <Karl.Bristav@gmail.com@d20b571a-298d-363c-a098-d6b3e227b0f4>
2011-05-17 13:05:00 +0000

    Imported the libraries directly into the project (made sure beforehand that the licenses approved. Also implemented multi-platform support.

--- trunk/gentlemen/src/core/Main.java
+++ trunk/gentlemen/src/core/Main.java
@@ -1,12 +1,31 @@
 package core;
 
+import java.io.File;
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
+
+		if (org.lwjgl.LWJGLUtil.getPlatform() == org.lwjgl.LWJGLUtil.PLATFORM_LINUX) {
+			System.setProperty("org.lwjgl.librarypath",
+					System.getProperty("user.dir") + File.separator
+							+ "lwjgl-2.7.1" + File.separator + "native"
+							+ File.separator + "linux");
+		} else if (org.lwjgl.LWJGLUtil.getPlatform() == org.lwjgl.LWJGLUtil.PLATFORM_MACOSX) {
+			System.setProperty("org.lwjgl.librarypath",
+					System.getProperty("user.dir") + File.separator
+							+ "lwjgl-2.7.1" + File.separator + "native"
+							+ File.separator + "macosx");
+		} else if (org.lwjgl.LWJGLUtil.getPlatform() == org.lwjgl.LWJGLUtil.PLATFORM_WINDOWS) {
+			System.setProperty("org.lwjgl.librarypath",
+					System.getProperty("user.dir") + File.separator
+							+ "lwjgl-2.7.1" + File.separator + "native"
+							+ File.separator + "windows");
+		}
+
 		Manager.start();
 	}
-
 }
