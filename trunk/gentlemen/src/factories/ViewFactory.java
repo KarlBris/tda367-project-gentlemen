3089dfa2289a95aa3eeef2fb8492b11fad37b933
gustav128@gmail.com <gustav128@gmail.com@d20b571a-298d-363c-a098-d6b3e227b0f4>
2011-05-17 08:00:52 +0000

    MainController, MainModel and View2D are now singletons and the factories do not create new instances.

--- trunk/gentlemen/src/factories/ViewFactory.java
+++ trunk/gentlemen/src/factories/ViewFactory.java
@@ -1,13 +1,12 @@
 package factories;
 
-import model.IMainModel;
 import view.IView;
 import view.twodimensions.View2D;
 
 public class ViewFactory {
 
-	public static IView get(final IMainModel mainModel) {
-		return new View2D(mainModel);
+	public static IView get() {
+		return View2D.get();
 	}
 
 }
